f3bfaeb29621c6534c9e1641c96a32324289aed7
jaein <dlawodls96@naver.com>
2017-05-08 17:42:52 +0900

    비디오 추가

--- app/src/main/java/com/example/victory/balan_swing/LoginActivity.java
+++ app/src/main/java/com/example/victory/balan_swing/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.victory.balan_swing;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ContextThemeWrapper;
 import android.util.Log;
@@ -43,10 +47,16 @@ public class LoginActivity extends AppCompatActivity {
 
     final int REQUEST_SIGNUP = 1;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        verifyStoragePermissions(this);
 
         select = getResources().getStringArray(R.array.dialog_select);
         set = getResources().getStringArray(R.array.dialog_set);
@@ -168,6 +178,17 @@ public class LoginActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
                 accountSpinner.setEnabled(true);
             }
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (writePermission != PackageManager.PERMISSION_GRANTED
+                || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
         }
     }
 }
--- app/src/main/java/com/example/victory/balan_swing/PersonalActivity.java
+++ app/src/main/java/com/example/victory/balan_swing/PersonalActivity.java
@@ -1,16 +1,39 @@
 package com.example.victory.balan_swing;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Toast;
 
-public class PersonalActivity extends AppCompatActivity {
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class PersonalActivity extends AppCompatActivity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener {
+
+    SurfaceHolder mSh;
+    String sdRootPath;
+    MediaPlayer mPlayer;
+
+    boolean StartNStop = true;
+    boolean mFirst = true;
+
+    PlaybackParams params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+        SurfaceView sv = (SurfaceView) findViewById(R.id.svVideo);
+        mSh = sv.getHolder();
+        mSh.addCallback(this);
+
     }
 
     public void btnClick(View view) {
@@ -23,4 +46,158 @@ public class PersonalActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    public void loadVideoSource() {
+
+        sdRootPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        //자기꺼 주석 풀어서 쓰기!
+
+        //String filePath = sdRootPath+"/DCIM/Camera"+"/20170426_162440.mp4"; //영서 오빠 영상
+        //String filePath = sdRootPath+"/DCIM/Camera"+"/20170314_225610.mp4"; // 종현이 영상
+        String filePath = sdRootPath + "/DCIM/Camera" + "/20170501_161246.mp4"; // 종현이 영상
+
+
+        try {
+
+            mPlayer = new MediaPlayer();
+
+            //error 1, 2147483648 나는 코드
+            //mPlayer.setDataSource(filePath);
+
+            //에러 수정 코드
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            mPlayer.setDataSource(fileInputStream.getFD());
+            fileInputStream.close();
+
+            mPlayer.setDisplay(mSh);
+
+            mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    if (StartNStop) {
+                        mPlayer.start();
+                    } else {
+                        mPlayer.pause();
+                    }
+                }
+            });
+
+            mPlayer.prepareAsync();
+
+
+        } catch (IOException e) {
+
+            return;
+
+        }
+
+    }
+
+
+    public void onPrepared(MediaPlayer mp) {
+
+        if (mFirst) {
+
+            mFirst = false;
+
+            mPlayer.start();
+
+            Toast.makeText(getApplicationContext(), "비디오 소스 로드 완료", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        loadVideoSource();
+
+        mSh.setFixedSize(mPlayer.getVideoWidth(), mPlayer.getVideoHeight());
+
+    }
+
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.buttonPlay:
+                mPlayer.start();
+                break;
+
+            case R.id.buttonPause:
+                mPlayer.pause();
+                break;
+
+            case R.id.buttonStop:
+                deletePlayer();
+                StartNStop = false;
+                loadVideoSource();
+                StartNStop = true;
+                break;
+
+            case R.id.buttonSlow:
+                SlowPlayer();
+                break;
+
+            case R.id.buttonReturn:
+                ReturnVelocity();
+                break;
+
+            case R.id.buttonFast:
+                FastPlayer();
+                break;
+
+        }
+
+    }
+
+    private void FastPlayer() {
+        //재생속도
+        params = mPlayer.getPlaybackParams();
+
+        float speed = 1.5f;
+        mPlayer.setPlaybackParams(params.setSpeed(speed));
+    }
+
+    private void ReturnVelocity() {
+        //재생속도
+        params = mPlayer.getPlaybackParams();
+
+        float speed = 1.0f;
+        mPlayer.setPlaybackParams(params.setSpeed(speed));
+    }
+
+    private void SlowPlayer() {
+        //재생속도
+        params = mPlayer.getPlaybackParams();
+
+        float speed = 0.5f;
+        mPlayer.setPlaybackParams(params.setSpeed(speed));
+    }
+
+
+    public void deletePlayer() {
+
+        if (mPlayer != null) {
+
+            mPlayer.stop();
+
+            mPlayer.release();
+
+            mPlayer = null;
+
+        }
+
+    }
+
 }
