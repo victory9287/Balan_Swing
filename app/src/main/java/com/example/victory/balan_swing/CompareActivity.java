f4bd73e537575af6da9c68a008b6af3811d460b4
K <zappyboy@konkuk.ac.kr>
2017-05-24 16:37:49 +0900

    막대그래프

--- app/src/main/java/com/example/victory/balan_swing/CompareActivity.java
+++ app/src/main/java/com/example/victory/balan_swing/CompareActivity.java
@@ -1,11 +1,16 @@
 package com.example.victory.balan_swing;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +22,17 @@ public class CompareActivity extends AppCompatActivity {
     int[] profileID = {R.drawable.profile_m, R.drawable.profile_w, R.drawable.profile_m};
     String[] select_sample;
 
+    private LinearLayout mPDRField;
+    MYView mView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare);
         init();
+        mView = new MYView(this);
+        mPDRField = (LinearLayout)findViewById(R.id.personalGraphView);
+        mPDRField.addView(mView);
     }
 
     public void init() {
@@ -60,6 +71,68 @@ public class CompareActivity extends AppCompatActivity {
                 startActivity(intent);
                 // 설정 바꾸면 finish
                 // 아니면 그대로
+
+        }
+    }
+    class MYView extends View {
+        int width;
+        int height;
+        int w10;
+        int h10;
+
+        public MYView(Context context) {
+            super(context);
+        }
+
+        public void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            width = w;
+            height = h;
+            w10 = width / 10;
+            h10 = height / 10;
+        }
+
+        public void onDraw(Canvas canvas) {
+            Paint greenPaint = new Paint();
+            greenPaint.setColor(0xff00964c);
+            greenPaint.setStrokeWidth(11);
+
+            Paint blackPaint = new Paint();
+            blackPaint.setColor(Color.BLACK);
+            blackPaint.setStrokeWidth(11);
+
+            Paint linePaint = new Paint();
+            linePaint.setColor(0xff404040);
+            linePaint.setTextSize(50);
+
+            int basex;
+            int basey;
+
+            double LF, RF = 0.0;
+            LF = 5.0;
+            RF = 8.0;
+
+
+            basex = w10;
+            basey = h10;
+            canvas.drawLine(basex, basey, basex, height - basey, blackPaint);
+            canvas.drawLine(basex, height - basey, width - basex, height - basey, blackPaint);
+            canvas.drawLine(width - basex, height - basey, width - basex, basey, blackPaint);
+            canvas.drawLine(width - basex, basey, basex, basey, blackPaint);
+            canvas.drawLine(basex * 2, basey * 3, basex * 8, basey * 3, blackPaint);
+
+
+            canvas.drawRect(basex * 2, basey * 3, basex * 4, basey * (int) LF, greenPaint);
+            canvas.drawText("  왼발", basex * 2, basey * 2, linePaint);
+
+            canvas.drawRect(basex * 6, basey * 3, basex * 8, basey * (int) RF, greenPaint);
+            canvas.drawText(" 오른발", basex * 6, basey * 2, linePaint);
+
+            canvas.drawLine(basex * 3, basey * (int) LF, basex * 7, basey * (int) RF, blackPaint);
+            canvas.drawCircle(basex * 3, basey * (int) LF, basex / 5, blackPaint);
+            canvas.drawCircle(basex * 7, basey * (int) RF, basex / 5, blackPaint);
+
+
         }
     }
 }
--- app/src/main/java/com/example/victory/balan_swing/PersonalActivity.java
+++ app/src/main/java/com/example/victory/balan_swing/PersonalActivity.java
@@ -1,6 +1,10 @@
 package com.example.victory.balan_swing;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.media.MediaPlayer;
 import android.media.PlaybackParams;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -25,6 +30,9 @@ public class PersonalActivity extends AppCompatActivity implements SurfaceHolder
 
     PlaybackParams params;
 
+    private LinearLayout mPDRField;
+    MYView mView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +41,9 @@ public class PersonalActivity extends AppCompatActivity implements SurfaceHolder
         SurfaceView sv = (SurfaceView) findViewById(R.id.svVideo);
         mSh = sv.getHolder();
         mSh.addCallback(this);
+        mView = new MYView(this);
+        mPDRField = (LinearLayout)findViewById(R.id.personalGraphView);
+        mPDRField.addView(mView);
 
     }
 
@@ -200,5 +211,67 @@ public class PersonalActivity extends AppCompatActivity implements SurfaceHolder
         }
 
     }
+    class MYView extends View {
+        int width;
+        int height;
+        int w10;
+        int h10;
+
+        public MYView(Context context) {
+            super(context);
+        }
+
+        public void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            width = w;
+            height = h;
+            w10 = width / 10;
+            h10 = height / 10;
+        }
+
+        public void onDraw(Canvas canvas) {
+            Paint greenPaint = new Paint();
+            greenPaint.setColor(0xff00964c);
+            greenPaint.setStrokeWidth(11);
+
+            Paint blackPaint = new Paint();
+            blackPaint.setColor(Color.BLACK);
+            blackPaint.setStrokeWidth(11);
+
+            Paint linePaint = new Paint();
+            linePaint.setColor(0xff404040);
+            linePaint.setTextSize(50);
+
+            int basex;
+            int basey;
+
+            double LF, RF = 0.0;
+            LF = 5.0;
+            RF = 8.0;
+
+
+            basex = w10;
+            basey = h10;
+            canvas.drawLine(basex, basey, basex, height - basey, blackPaint);
+            canvas.drawLine(basex, height - basey, width - basex, height - basey, blackPaint);
+            canvas.drawLine(width - basex, height - basey, width - basex, basey, blackPaint);
+            canvas.drawLine(width - basex, basey, basex, basey, blackPaint);
+            canvas.drawLine(basex * 2, basey * 3, basex * 8, basey * 3, blackPaint);
+
+
+            canvas.drawRect(basex * 2, basey * 3, basex * 4, basey * (int) LF, greenPaint);
+            canvas.drawText("  왼발", basex * 2, basey * 2, linePaint);
+
+            canvas.drawRect(basex * 6, basey * 3, basex * 8, basey * (int) RF, greenPaint);
+            canvas.drawText(" 오른발", basex * 6, basey * 2, linePaint);
+
+            canvas.drawLine(basex * 3, basey * (int) LF, basex * 7, basey * (int) RF, blackPaint);
+            canvas.drawCircle(basex * 3, basey * (int) LF, basex / 5, blackPaint);
+            canvas.drawCircle(basex * 7, basey * (int) RF, basex / 5, blackPaint);
+
+
+        }
+    }
+
 
 }
